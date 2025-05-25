52f872aad1085c74240c427c5bec1ac3e215c282
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-05-25 19:07:15 +0100

    Add close button to alerts

--- src/myapp/frontend/layout/alert.cljs
+++ src/myapp/frontend/layout/alert.cljs
@@ -1,22 +1,32 @@
-(ns myapp.frontend.layout.alert)
+(ns myapp.frontend.layout.alert
+  (:require [re-frame.core :as rf]))
 
 (defn alert-error [message]
   [:div.alert.pico-background-red-50 {:role "alert"}
    [:span.material-symbols-outlined.pico-color-red-500
     "error"]
    [:span.pico-color-red-500
-    message]])
+    message]
+   [:button.close-btn
+    {:on-click #(re-frame.core/dispatch [:app/alert-clear])}
+    [:span.material-symbols-outlined.pico-primary "close"]]])
 
 (defn alert-warning [message]
   [:div.alert.pico-background-amber-50 {:role "alert"}
    [:span.material-symbols-outlined.pico-color-amber-500
     "error"]
    [:span.pico-color-amber-500
-    message]])
+    message]
+   [:button.close-btn
+    {:on-click #(re-frame.core/dispatch [:app/alert-clear])}
+    [:span.material-symbols-outlined.pico-primary "close"]]])
 
 (defn alert-success [message]
   [:div.alert.pico-background-jade-50 {:role "alert"}
    [:span.material-symbols-outlined.pico-color-green-500
     "check_circle"]
    [:span.pico-color-green-500
-    message]])
+    message]
+   [:button.close-btn
+    {:on-click #(re-frame.core/dispatch [:app/alert-clear])}
+    [:span.material-symbols-outlined.pico-primary "close"]]])
