7299f1952b41c5e7c912ac04dc2ed9e0cf1bd5a9
Paul Cockrell <paul.cockrell@oxfordmedicalsimulation.com>
2025-06-03 17:22:07 +0100

    Update servo page ui"

--- src/myapp/frontend/views/servo.cljs
+++ src/myapp/frontend/views/servo.cljs
@@ -4,7 +4,28 @@
 
 (defn servo []
   [layout/layout
-   [:div [:h1 "Devices - Servo"]
-    [:button {:on-click #(do
-                           (.preventDefault %)
-                           (rf/dispatch [:arduino/servo-move {:angle (rand-int 179)}]))} "Move servo"]]])
+   [:<>
+    [:section
+     [:hgroup
+      [:div.heading-icon
+       [:span.material-symbols-outlined "mode_fan"]]
+      [:p "A servo is a small motor with a control circuit that lets you precisely move it to a specific angle, often used to rotate or position things like levers, arms, or sensors."]
+      [:p "Move the slider to change the angle of the servo"]]]
+
+    [:section
+     [:fieldset
+      [:label {:for "servo-angle"} "Servo angle"]
+      [:input {:id "servo-angle"
+               :name "servo-angle"
+               :type "range"
+               :list "servo-angles"
+               :min "0"
+               :max "180"
+               :step "20"
+               :default-value "0"
+               :style (js-obj "--pico-selected-ratio" "25%")
+               :on-change (fn [e]
+                            (rf/dispatch [:arduino/servo-move {:angle (js/parseInt (.. e -target -value))}]))}]
+      [:div.datalist
+       (for [label ["0" "180"]]
+         [:span {:key label} label])]]]]])
